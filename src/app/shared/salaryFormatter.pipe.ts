9e01e44d8861391d1164c0247e8a29bafc4db612
mhtengaten <mhtengaten@gmail.com>
2018-06-03 20:29:53 +0200

    now display liveEarnings and liveTimer correctly

--- src/app/shared/salaryFormatter.pipe.ts
+++ src/app/shared/salaryFormatter.pipe.ts
@@ -17,20 +17,20 @@ export class SalaryFormatterPipe implements PipeTransform {
         let returningString;
         if (value > 100000) {
             let salary = Math.round(value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-            returningString = `<span class="salary--noticeable">${salary}</span>`;
+            returningString = `<span class="livesalary--noticeable">${salary}</span>`;
         } else if (value > 1000) {
             let salary = value.toFixed(2);
             let salarySplited = salary.split('.');
             salarySplited[0] = salarySplited[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-            returningString = `<span class="salary--noticeable">${salarySplited[0]},</span><span class="salary--negligible">${salarySplited[1]}</span>`;
+            returningString = `<span class="livesalary--noticeable">${salarySplited[0]},</span><span class="livesalary--negligible">${salarySplited[1]}</span>`;
         } else {
             let salary = value.toFixed(5);
             let salarySplited = salary.split('.');
             salarySplited[0] = salarySplited[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
             salarySplited.push(salarySplited[1].slice(0,2));
             salarySplited.push(salarySplited[1].slice(2,3));
-            returningString = `<span class="salary--noticeable">${salarySplited[0]},</span><span class="salary--negligible">${salarySplited[2]}<span class="salary--evenMoreNegligible">${salarySplited[3]}</span></span>`;
+            returningString = `<span class="livesalary--noticeable">${salarySplited[0]},</span><span class="livesalary--negligible">${salarySplited[2]}<span class="livesalary--evenMoreNegligible">${salarySplited[3]}</span></span>`;
         }
-    return `<span class="yolo">${returningString} €</span>`;
+    return `<span class="livesalary">${returningString} €</span>`;
   }
 }
--- src/app/stats-board/component/live-earnings/live-earnings.component.ts
+++ src/app/stats-board/component/live-earnings/live-earnings.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-live-earnings',
   templateUrl: './live-earnings.component.html',
   styleUrls: ['./live-earnings.component.css']
 })
-export class LiveEarningsComponent implements OnInit {
+export class LiveEarningsComponent implements OnInit, OnChanges {
   @Input() liveSalaryPerSecond: number;
   @Input() liveTimer: number;
+  chronometer: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    if (this.liveTimer != undefined) {
+      this.chronometer = new Date(this.liveTimer * 1000).toISOString().substr(11, 8);
+    }
   }
 
 }
