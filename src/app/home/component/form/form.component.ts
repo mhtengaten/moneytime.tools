f81d980cc9f9f88033a9149510e0f47d721560d6
mhtengaten <mhtengaten@gmail.com>
2018-06-03 13:42:38 +0200

    angular 5 to 6, creating form, errors, navigation

--- src/app/home/component/form/form.component.ts
+++ src/app/home/component/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'home-form',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  salaryForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.salaryForm = new FormGroup({
+      'salary': new FormControl(null, [
+        Validators.required,
+        this.valueTooLow.bind(this),
+        this.valueTooHigh.bind(this),
+        this.valueIsNotANumber.bind(this)
+      ])
+    });
+
+    this.salaryForm.statusChanges.subscribe(
+      (status) => {
+        console.log(status);
+        console.log(this.salaryForm)
+      }
+    );
+  }
+
+  valueTooLow(control: FormControl): {[s: string]: boolean} {
+    if (control.value < 100) {
+      return {'valueTooLow': true};
+    }
+    return null;
+  }
+
+  valueTooHigh(control: FormControl): {[s: string]: boolean} {
+    if (control.value > 1000000000) {
+      return {'valueTooHigh': true};
+    }
+    return null;
+  }
+
+  valueIsNotANumber(control: FormControl): {[s: string]: boolean} {
+    const onlyNumbersRegex = /^[0-9]*$/;
+    if (!onlyNumbersRegex.test(control.value)) {
+      return {'valueIsNotANumber': true};
+    }
+    return null;
+  }
+
+  onSubmit() {
+    const salary = this.salaryForm.get('salary').value;
+    if(this.salaryForm.valid) {
+      this.router.navigate(['salary', salary]);
+    };
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './container/home.component';
 import { FormComponent } from './component/form/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
-  declarations: [HomeComponent, FormComponent],
+  declarations: [
+    HomeComponent,
+    FormComponent
+  ],
   exports:[
     HomeComponent
   ]
